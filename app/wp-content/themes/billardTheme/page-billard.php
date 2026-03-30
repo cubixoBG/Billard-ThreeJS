68807702b9b9595eb4ed88756e7453f57b5c424d
cubixo <cubixo2302@gmail.com>
2026-03-30 09:57:35 +0200

    mise en place des fichiers pour le jeu en threejs

---
+++ app/wp-content/themes/billardTheme/page-billard.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * billard
+ */
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Billard3D - jeu</title>
+</head>
+<body>
+    <p>jeu de billard</p>
+</body>
+</html>
